bd68cec751246d81e8f988ca5b90d3412ce0476a
azeezabass2005 <azeezabass2005@gmail.com>
2026-04-25 21:38:43 +0100

    chore: cors added to server

--- server/src/main.rs
+++ server/src/main.rs
@@ -1,4 +1,5 @@
-use axum::{Router, routing::post};
+use axum::{Router, http::{HeaderValue, Method}, routing::post};
+use tower_http::cors::{Any, CorsLayer};
 
 use crate::api::generate_semantic_data;
 
@@ -10,8 +11,17 @@ pub mod api;
 #[tokio::main]
 async fn main() {
     println!("MDF - The Maldives for PDFs");
+    let cors = CorsLayer::new()
+        .allow_methods([Method::POST, Method::GET, Method::PATCH])
+        .allow_origin([
+            "http://localhost:5173".parse::<HeaderValue>().unwrap(),
+            "https://mdf.thefola.dev".parse::<HeaderValue>().unwrap()
+        ])
+        .allow_headers(Any);
+
     let router = Router::new()
-        .route("/infer_semantics", post(generate_semantic_data));
+        .route("/infer_semantics", post(generate_semantic_data))
+        .layer(cors);
     let listener = tokio::net::TcpListener::bind("0.0.0.0:4000").await.unwrap();
     println!("Listening on port: {:?}", listener.local_addr());
     axum::serve(listener, router).await.unwrap();
